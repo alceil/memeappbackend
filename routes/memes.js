7c90a6188460fd94f69a69ea923b49553f577641
alceil <ashishshajisrampickal@gmail.com>
2020-09-01 11:51:13 +0530

    finished addmemes

--- routes/memes.js
+++ routes/memes.js
@@ -4,10 +4,20 @@ const AddMeme=require('../models/AddMeme.model');
 const GenMeme=require('../models/genmeme.model');
 const NewCat=require('../models/newcat.model');
 
-router.post('/addMeme',(req,res)
+router.post('/addMeme',async (req,res)=>
 {
     const addMeme = new AddMeme({
         imgUrl:req.body.imgUrl,
         catname:req.body.catname,
-    }) 
-})
+    });
+    try
+    {
+        const savedMeme = await addMeme.save();
+        res.json(savedMeme);
+
+    }catch(err){
+        res.json({msg:err});
+
+    }
+});
+module.exports = router;
