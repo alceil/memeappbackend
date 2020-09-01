7e2be10c5e2ecccda46139106a6089d7e32114f0
alceil <ashishshajisrampickal@gmail.com>
2020-09-01 13:00:22 +0530

    added app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = require('express');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const memeRoutes = require('./routes/memes'); 
--- routes/memes.js
+++ routes/memes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const AddMeme=require('../models/AddMeme.model');
 const GenMeme=require('../models/genmeme.model');
 const NewCat=require('../models/newcat.model');
