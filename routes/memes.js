0e0c1b2ff988274db82c3727af40df7ddb8b1411
alceil <ashishshajisrampickal@gmail.com>
2020-09-10 10:02:31 +0530

    adding new changes

--- routes/memes.js
+++ routes/memes.js
@@ -2,71 +2,62 @@ const express = require('express');
 const router = express.Router();
 const AddMeme = require('../models/AddMeme.model');
 const GenMeme = require('../models/genmeme.model');
+const aws = require('aws-sdk');
 const NewCat = require('../models/newcat.model');
+const multerS3 = require('multer-s3');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./uploads");
-    },
-    filename:(req,file,cb)=>{
-        cb(null,Date.now().toString()+".jpg");
-    }
+// const storage = multer.diskStorage({
+//     destination:(req,file,cb)=>{
+//         cb(null,"./uploads");
+//     },
+//     filename:(req,file,cb)=>{
+//         cb(null,Date.now().toString()+".jpg");
+//     }
+// });
+
+// const FileFilter = (req,file,cb)=>{
+//     if(file.mimetype=='image/jpg'||file.mimetype=='image/png'){
+//         cb(null,true);
+//     }
+//         else{
+//          cb(null,false);   
+//         }
+//     };
+
+//  const upload = multer({
+//      storage:storage,
+//      limits:{
+//          fileSize:1024 * 1024 * 6,
+//      }
+//  });
+
+aws.config.update({
+    accessKeyId: process.env.aws_access_key_id,
+    secretAccessKey: process.env.aws_secret_access_key
 });
 
-const FileFilter = (req,file,cb)=>{
-    if(file.mimetype=='image/jpg'||file.mimetype=='image/png'){
-        cb(null,true);
-    }
-        else{
-         cb(null,false);   
-        }
-    };
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'cgsteam',
+      acl: 'public-read',
+      key: function (request, file, cb) {
+        console.log(file);
+        cb(null, file.originalname);
+      }
+    })
+  }).array('upload', 1);
 
- const upload = multer({
-     storage:storage,
-     limits:{
-         fileSize:1024 * 1024 * 6,
-     }
- });
+
+  
+app.post('/upload', function (request, response, next) {
+  
+});
  
-<<<<<<< HEAD
  router.route('/add/image').post(upload.single("img"),async (req,res)=>{
-    //  AddMeme.findOneAndUpdate({username:req.body.name},
-    //     {
-    //         $set:{
-    //             imgUrl:req.file.path,
-    //         }
-    //     })
-=======
-router.route('/add/image').post(upload.single("img"),async (req,res)=>{
-    const addMeme = new AddMeme({
-        imgUrl: req.file.path,
-    });
-    try {
-//         const savedMeme = await addMeme.save();
-        res.json(addMeme);
 
-    } catch (err) {
-        res.json({ msg: err });
-
-    }
- });
-
->>>>>>> f647714567d58c7645501b7809a3f500b6bafaef
-
-
-    const addMeme = new AddMeme({
-        imgUrl: req.file.path,
-    });
-    try {
-        const savedMeme = await addMeme.save();
-        res.json(savedMeme);
-
-    } catch (err) {
-        res.json({ msg: err });
-
-    }
+ 
  });
 
 router.post('/addMeme', async (req, res) => {
