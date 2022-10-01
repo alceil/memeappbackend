0ecdd4cbe1cf0ac7d7a1862895886a7fd3c21fd5
alceil <ashishshajisrampickal@gmail.com>
2022-10-01 23:09:33 +0530

    make it better

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const memeRoutes = require('./routes/memes'); 
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,4 +14,4 @@ app.get('/',(req,res)=>{
     res.send('We are legion')
 });
 mongoose.connect('mongodb+srv://nemesisx:nemesisx@kindi.c110p.mongodb.net/kindi?retryWrites=true&w=majority',{ useFindAndModify: false , useNewUrlParser: true,useUnifiedTopology: true },()=>console.log('Connected to db'));
-app.listen(3000);
+app.listen(PORT);
